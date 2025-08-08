fa4d9fe7152b23877d5a831eb910121118563e66
danghuynhkhanh <danghuynhkhanh@gmail.com>
2025-08-08 19:18:02 +0700

    admin

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+# -------------------
+# Route /admin94 không cần mật khẩu
+# -------------------
+@app.route('/admin94')
+def admin94():
+    return render_template('generate_key.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
